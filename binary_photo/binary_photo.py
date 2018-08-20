af705ebd4462c0d403bff68e86352a1a36d826b1
Yacin Tmimi <yacintmimi@gmail.com>
2018-08-20 04:25:10 -0400

    script converts an image into hex image. need to create functions to make it easier to implement

--- binary_photo/binary_photo.py
+++ binary_photo/binary_photo.py
@@ -4,13 +4,12 @@ from PIL import Image, ImageColor
 from aggdraw import Draw, Font
 
 WHITE = (255, 255, 255)
-
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 IMG_PATH = os.path.join(BASE_PATH, 'images')
 ROBOTO = os.path.join(BASE_PATH, 'fonts', 'Roboto', 'Roboto-Thin.ttf')
-print(ROBOTO)
-img = Image.open(os.path.join(IMG_PATH, 'Red_Box.png'))
-img.show()
+SCALE = (35,10)
+
+img = Image.open(os.path.join(IMG_PATH, 'random.png'))
 img_width, img_height = img.size
 
 image_data = {}
@@ -18,25 +17,9 @@ image_data = {}
 def convertBinary(number):
     return '{0:08b}'.format(number)
 
-# for i in range(img_width):
-#     for j in range(img_height):
-#         r,g,b = img.getpixel((i, j))
-#         text = f'{convertBinary(r)}{convertBinary(g)}{convertBinary(b)}'
-#         image_data[(i, j)] = {'rgb':(r,g,b), 'text':text}
-
-first_pixel = (0, 0)
-r,g,b = img.getpixel(first_pixel)
-text_r = f'{convertBinary(r)}'
-text_g = f'{convertBinary(g)}'
-text_b = f'{convertBinary(b)}'
-image_data[first_pixel] ={'rgb':(r,g,b), 'text':{'r':text_r, 'g':text_g, 'b':text_b}}
-
-# print(image_data)
-
-# numpy_img = np.array(img.getpixel((0, 0)))
-# print(numpy_img.shape)
-# print(numpy_img.vectorize(convertBinary, otypes=str))
-# print(numpy_img)
+def rgb2hex(r,g,b):
+    hex = "#{:02x}{:02x}{:02x}".format(r,g,b)
+    return hex
 
 # Steps to the process
 # get the RGB data in each pixel stored in a dictionary (x,y) key
@@ -44,17 +27,32 @@ image_data[first_pixel] ={'rgb':(r,g,b), 'text':{'r':text_r, 'g':text_g, 'b':tex
 #create a new picture by addin the text of each row to a new img
 #reshape the new image to match the old image
 
-new_img = Draw("RGB", (450,300), WHITE)
 
-# font takes a color,
-roboto_font = Font(color=image_data[first_pixel]['rgb'], file=ROBOTO,size=100)
-#text takes a tuple coordinate, text to display, font
-# text(xy, text, font)
-new_img.text((0,0), image_data[first_pixel]['text']['r'], roboto_font)
-new_img.text((0,100), image_data[first_pixel]['text']['g'], roboto_font)
-new_img.text((0,200), image_data[first_pixel]['text']['b'], roboto_font)
-# converts the draw object to bytes
-img_byt = new_img.tobytes()
+def hex_pixel_image(img_data: dict, coordinate):
+    color = img_data[coordinate]['rgb']
+    font = Font(color, ROBOTO, 10)
+    draw_img = Draw("RGB", SCALE, WHITE)
+    draw_img.text((0,0), img_data[coordinate]['hex'], font)
+    byte_img = draw_img.tobytes()
+    return Image.frombytes("RGB", size=draw_img.size, data=byte_img)
 
-#converts the bytes to an image
-Image.frombytes("RGB", size=new_img.size, data=img_byt).show()
+for i in range(img_width):
+    for j in range(img_height):
+        # print(i,j)
+        r,g,b = img.getpixel((i, j))
+        text_hex = f'{rgb2hex(r,g,b)}'
+        image_data[(i,j)] ={'rgb':(r,g,b), 'hex':text_hex}
+        hex_img = hex_pixel_image(image_data, (i,j))
+        image_data[(i,j)]['img'] = hex_img
+
+width, height = image_data[(0,0)]['img'].size
+final_img = Image.new('RGB', (img_width*SCALE[0], img_height*SCALE[1]))
+for i in range(img_width):
+    for j in range(img_height):
+        img = image_data[(i,j)]['img']
+        box = (i*width, j*height)
+        final_img.paste(img, box)
+
+# final_img.thumbnail((64,64))
+final_img.show()
+# .save(os.path.join(IMG_PATH, 'hex.png'))

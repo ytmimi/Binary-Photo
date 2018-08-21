26f0a6b0d77ff08fe441451723779102406fbb01
Yacin Tmimi <yacintmimi@gmail.com>
2018-08-21 17:21:41 -0400

    Major Changes: Created a base class New Image. added several functions to manipulate rgb values. added options to change tint and shade of background and text values. added validatiors for tint and shade options. Also included a negative function. Finally extended the base class to the Hex_Image and Binary_Image classes

--- binary_photo/binary_photo.py
+++ binary_photo/binary_photo.py
@@ -1,12 +1,16 @@
 import os
+from math import floor
 from PIL import Image
 from aggdraw import Draw, Font
+
 
 WHITE = (255, 255, 255)
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 IMG_PATH = os.path.join(BASE_PATH, 'images')
-ROBOTO = os.path.join(BASE_PATH, 'fonts', 'Roboto', 'Roboto-Thin.ttf')
-SCALE = (35,10)
+ROBOTO_THIN = os.path.join(BASE_PATH, 'fonts', 'Roboto-Thin.ttf')
+ROBOTO_REGULAR = os.path.join(BASE_PATH, 'fonts', 'Roboto-Regular.ttf')
+TIMES = os.path.join(BASE_PATH, 'fonts', 'Times.ttc')
+VALID_OPTIONS = ['', 'tint', 'shade', 'negative']
 
 def number_to_binary(number):
     return f'{number:08b}'
@@ -15,50 +19,146 @@ def rgb_to_hex(rgb: tuple):
     r, g, b = rgb
     return f'#{r:02x}{g:02x}{b:02x}'
 
+def shade_rgb(rgb: tuple, shade_factor=.25):
+    '''Darkens the rgb value '''
+    newR = floor(rgb[0] * (1 - shade_factor))
+    newG = floor(rgb[1] * (1 - shade_factor))
+    newB = floor(rgb[2] * (1 - shade_factor))
+    return (newR, newG, newB)
 
-class Hex_Image():
-    def __init__(self, img: Image, scale=SCALE, rescale=True):
-        if rescale:
+def tint_rgb(rgb: tuple, tint_factor=.25):
+    '''Lightend the rbg vlalue'''
+    newR = floor(rgb[0] + (255 - rgb[0]) * tint_factor)
+    newG = floor(rgb[1] + (255 - rgb[1]) * tint_factor)
+    newB = floor(rgb[2] + (255 - rgb[2]) * tint_factor)
+    return (newR, newG, newB)
+
+def negative_rgb(rgb: tuple):
+    newR = 255 - rgb[0]
+    newG = 255 - rgb[1]
+    newB = 255 - rgb[2]
+    return (newR, newG, newB)
+
+
+class New_Image():
+    def __init__(self, img: Image, scale=(10,10), font=ROBOTO_REGULAR,
+                font_size=10,text='#', background_color='', text_color='',
+                tint_factor=.25, shade_factor=.25, resize=True):
+        '''
+        scale: (width, height) of each small picture that will be stiched together
+                to create the final image
+        font: path to a font file. Font files usually end in .ttf or .ttc
+        font_size: desired text size in pixels.
+        text: text to represent the pixel data
+        background_color: a string describig valid backgroung color options.
+                        default is '', which implies white. option: 'tint', 'shade'
+        text_color: a string describig valid text color options.
+                    default is '', which the pixel color. options: 'tint', 'shade'
+        tint_factor:if background_color or text_color is set to 'tint',
+                this will apply a shade to the pixel color before using it
+        shade_factor:if background_color or text_color is set to 'shade',
+                this will set theapply a shade to the pixel color before using it
+        rescale: Bool, detrmins if the image is scaled down before processing
+        '''
+        if resize:
             self.img = self.resize_img(img)
         else:
             self.img = img
-        self.scale = SCALE
+        self.scale = scale
+        self.font = font
+        self.font_size = font_size
+        self.text = text
+        self.background_color = self.validate_bg_color(background_color)
+        self.text_color = self.validate_text_color(text_color)
+        self.tint_factor = self.validate_tint_and_shade(tint_factor)
+        self.shade_factor = self.validate_tint_and_shade(shade_factor)
         self.img_width, self.img_height = self.img.size
         self.img_data = self.get_img_data()
 
-    def resize_img(self, img):
+
+    @staticmethod
+    def validate_bg_color(value):
+        if value in VALID_OPTIONS:
+            return value
+        else:
+            raise ValueError(f'background_color must be one of the follow: {VALID_OPTIONS}')
+
+    @staticmethod
+    def validate_text_color(value):
+        if value in VALID_OPTIONS:
+            return value
+        else:
+            raise ValueError(f'text_color must be one of the follow: {VALID_OPTIONS}')
+
+    @staticmethod
+    def validate_tint_and_shade(value):
+        if 0 <= value <=1:
+            return value
+        else:
+            raise ValueError(f'tint and shade values must be between 0 and 1')
+
+    @staticmethod
+    def resize_img(img):
         w, h = img.size
         if w > 256 or h > 256:
             img.thumbnail((256, 256))
-        return img
-
-    def append_image_data(self, image_data: dict, pixel_coordinates: tuple):
-        rgb = self.img.getpixel(pixel_coordinates)
-        text_hex = rgb_to_hex(rgb)
-        image_data[pixel_coordinates] = {'rgb':rgb, 'hex': text_hex,}
-        hex_img = self.hex_pixel_image(image_data, pixel_coordinates)
-        image_data[pixel_coordinates]['img'] = hex_img
-
-    def hex_pixel_image(self, image_data: dict, pixel_coordinates: tuple):
-        color = image_data[pixel_coordinates]['rgb']
-        text = image_data[pixel_coordinates]['hex']
-        img = self.create_pixel_img(SCALE, text, 10, color)
-        return Image.frombytes("RGB", size=img.size, data=img.tobytes())
-
-    def create_pixel_img(self, scale, text, font_size, font_color, background_color=WHITE):
-        font = Font(font_color, ROBOTO, font_size)
-        img = Draw('RGB', scale, background_color)
-        img.text((0,0), text, font)
         return img
 
     def get_img_data(self):
         image_data = {}
         for i in range(self.img_width):
             for j in range(self.img_height):
-                self.append_image_data(image_data, (i, j))
+                self.append_image_data(image_data, (i, j), self.pixel_image)
         return image_data
 
-    def new_image(self):
+    def append_image_data(self, image_data: dict, pixel_coordinates: tuple, img_func):
+        rgb = self.img.getpixel(pixel_coordinates)
+        text = self.text
+        image_data[pixel_coordinates] = {'rgb':rgb, 'text':text,}
+        img = img_func(image_data, pixel_coordinates)
+        image_data[pixel_coordinates]['img'] = img
+
+    def pixel_image(self, image_data: dict, pixel_coordinates: tuple):
+        '''Creates a new image based on the color data of a given pixel'''
+        color = image_data[pixel_coordinates]['rgb']
+        text = image_data[pixel_coordinates]['text']
+        img = self.create_pixel_img(self.scale, text, color)
+        return Image.frombytes("RGB", size=img.size, data=img.tobytes())
+
+    def set_background_color(self, color):
+        if self.background_color == 'tint':
+            return tint_rgb(color, self.tint_factor)
+        elif self.background_color == 'shade':
+            return shade_rgb(color, self.shade_factor)
+        elif self.background_color == 'negative':
+            return negative_rgb(color)
+        else:
+            return (255, 255, 255)
+
+    def set_text_color(self, color):
+        if self.text_color  == 'tint':
+            return tint_rgb(color, self.tint_factor)
+        elif self.text_color == 'shade':
+            return shade_rgb(color, self.shade_factor)
+        elif self.text_color == 'negative':
+            return negative_rgb(color)
+        else:
+            return color
+
+    def create_pixel_img(self, scale, text, color):
+        '''Creats a new canvas and draws text onto it if overridin'''
+        #define the font and the blank canvas
+        font = Font(self.set_text_color(color), self.font, self.font_size)
+        #defines the background color of the canvas
+        bg_color = self.set_background_color(color)
+        img = Draw('RGB', scale, bg_color)
+        #draw text on to the canvas by default only one line of text is drawn
+        #this method can be overriden to draw as much text as you want
+        #Note that the if the scale is not large enough not all the text will be displayed
+        img.text((0,0), text, font)
+        return img
+
+    def join_pixel_images(self):
         width, height = self.img_data[(0,0)]['img'].size
         new_img = Image.new('RGB', (self.img_width*self.scale[0],
                                     self.img_height*self.scale[1]))
@@ -69,7 +169,56 @@ class Hex_Image():
                 new_img.paste(img, box)
         return new_img
 
+
+class Hex_Image(New_Image):
+    def __init__(self, img, font_size=10, scale=(45,25), **kwargs):
+        super().__init__(img,font_size=font_size, scale=scale, **kwargs)
+
+    def append_image_data(self, image_data: dict, pixel_coordinates: tuple, img_func):
+        rgb = self.img.getpixel(pixel_coordinates)
+        #overides the text from the base class
+        text = rgb_to_hex(rgb)
+        image_data[pixel_coordinates] = {'rgb':rgb, 'text':text,}
+        img = img_func(image_data, pixel_coordinates)
+        image_data[pixel_coordinates]['img'] = img
+
+    def create_pixel_img(self, scale, text, color,):
+        '''Creats a new canvas and draws text onto it if overridin'''
+        font = Font(self.set_text_color(color), self.font, self.font_size)
+        bg_color = self.set_background_color(color)
+        img = Draw('RGB', scale, bg_color)
+        img.text((0,7), text, font)
+        return img
+
+
+class Binary_Image(New_Image):
+    def __init__(self, img, font_size=10, scale=(45,35), **kwargs):
+        super().__init__(img, font_size=font_size,scale=scale, **kwargs)
+
+    def append_image_data(self, image_data: dict, pixel_coordinates: tuple, img_func):
+        rgb = self.img.getpixel(pixel_coordinates)
+        text_r = number_to_binary(rgb[0])
+        text_g = number_to_binary(rgb[1])
+        text_b = number_to_binary(rgb[2])
+        image_data[pixel_coordinates] = {'rgb':rgb, 'text':{'r':text_r, 'g':text_g, 'b':text_b}}
+        img = img_func(image_data, pixel_coordinates)
+        image_data[pixel_coordinates]['img'] = img
+
+    def create_pixel_img(self, scale, text, color,):
+        '''Creats a new canvas and draws text onto it if overridin'''
+        font = Font(self.set_text_color(color), self.font, self.font_size)
+        bg_color = self.set_background_color(color)
+        img = Draw('RGB', scale, bg_color)
+        img.text((0,0), text['r'], font)
+        img.text((0,10), text['g'], font)
+        img.text((0,20), text['b'], font)
+        return img
+
+
+
+
 if __name__ == '__main__':
-    img = Image.open(os.path.join(IMG_PATH, 'bad_smile.png'))
-    hex_img = Hex_Image(img, rescale=False)
-    hex_img.new_image().show()
+    img = Image.open(os.path.join(IMG_PATH, 'random.png'))
+    img = Hex_Image(img, background_color='tint', tint_factor=.9,
+                    text_color='shade',shade_factor=.5)
+    img.join_pixel_images().show()
